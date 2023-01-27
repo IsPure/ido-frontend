300c452ef397846aff559ced5ebf7b604fa8dbe9
Isaac Pure <isaacpure@gmail.com>
2023-01-27 17:15:31 -0600

    formatted react-router pages; confirmed router is working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
