f93e64aef745ac76adcd3843f8e9e10ea60e279b
Isaac Pure <isaacpure@gmail.com>
2023-01-30 10:49:17 -0600

    added react redux slices for auth. created client side routes to connect to backend

---
+++ src/redux/slices/authSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { isAuth: false };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    authenticateUser: (state) => {
+      state.isAuth = true;
+    },
+    unauthenticateUser: (state) => {
+      state.isAuth = false;
+    },
+  },
+});
+
+export const { authenticateUser, unauthenticateUser } = authSlice.actions;
+export default authSlice.reducer;
